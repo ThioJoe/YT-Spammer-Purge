512b403f2912c8020e7a6a9a21a5061deb25c999
MDP43140 <68391650+MDP43140@users.noreply.github.com>
2021-11-04 14:32:51 +0700

    Update YouTubeSpammerPurge.py
    Change client secret file name error from "client_secrets.json" to "CLIENT_SECRETS_FILE" variable

--- YouTubeSpammerPurge.py
+++ YouTubeSpammerPurge.py
@@ -86,7 +86,7 @@ TOKEN_FILE = 'token.pickle'
 
 # Check if client_secrets.json file exists, if not give error
 if not os.path.exists(CLIENT_SECRETS_FILE):
-  print("\n ------------- ERROR: client_secrets.json file not found! ------------- ")
+  print("\n ------------- ERROR: "+CLIENT_SECRETS_FILE+" file not found! ------------- ")
   print(" Make sure it is placed in the same folder as the program, and is spelled as above \n")
   print(" ----- Or: Did you create a Google Cloud Platform Project to access the API? ----- ")
   print(" ------ See section with instructions on obtaining an API Key at this page: ------- ")
