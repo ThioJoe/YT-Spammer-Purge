154a7cd6f7520c7c6ca1b558be4e65cdf9a625de
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2021-11-26 13:15:10 -0700

    Update version number

--- YouTubeSpammerPurge.py
+++ YouTubeSpammerPurge.py
@@ -35,7 +35,7 @@
 ### IMPORTANT:  I OFFER NO WARRANTY OR GUARANTEE FOR THIS SCRIPT. USE AT YOUR OWN RISK.
 ###             I tested it on my own and implemented some failsafes as best as I could,
 ###             but there could always be some kind of bug. You should inspect the code yourself.
-version = "1.6.0"
+version = "1.6.1"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 from gui import *
