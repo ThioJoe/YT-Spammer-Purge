fb5fe3b1c0b1c364fea606b31cad792c8374e18a
lolpro11 <89674666+lolpro11@users.noreply.github.com>
2021-12-24 15:05:13 -0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-py=$(python --version)
 command -v python >/dev/null 2>&1 || { pacman -S python; dnf install python3; apt install python; zypper install python3; emerge python;}
-command -v pip >/dev/null 2>&1 || { pacman -S python-pip; apt install pip; zypper install python3-pip; emerge --ask dev-python/pip;}
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 curl https://codeload.github.com/ThioJoe/YouTube-Spammer-Purge/tar.gz/refs/tags/v2.2.5 -o yt-spammer.tar.gz
+python get-pip.py
 tar -xzf yt-spammer.tar.gz
 cd YouTube-Spammer-Purge-2.2.5/
 pip install -r requirements.txt
