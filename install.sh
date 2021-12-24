31fe2eefe40b189d245dce495d1b5e6b2ccfcc9b
lolpro11 <89674666+lolpro11@users.noreply.github.com>
2021-12-24 15:10:27 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 curl https://codeload.github.com/ThioJoe/YouTube-Spammer-Purge/tar.gz/refs/tags/v2.2.5 -o yt-spammer.tar.gz
 python get-pip.py
 tar -xzf yt-spammer.tar.gz
+rm yt-spammer.tar.gz
+rm get-pip.py
 cd YouTube-Spammer-Purge-2.2.5/
 pip install -r requirements.txt
 echo "Dependencies installed!"
+echo "Starting Program..."
