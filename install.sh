49e7c17d82db06e7ef0a601b0b5f0deb1979ba1f
lolpro11 <lolpro11@hostname.localdomain>
2021-12-24 19:10:14 -0800

    added required lib

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #checks for python; if not installed will install it
 command -v python >/dev/null 2>&1 || { pacman -S python; dnf install python3; apt install python3; zypper install python3; emerge python;}
+#install tkinter, a dependency
+apt-get install python3-tk; pacman -Syu tk; dnf install python3-tkinter; zypper install python3-tk
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 curl https://codeload.github.com/ThioJoe/YouTube-Spammer-Purge/tar.gz/refs/tags/v2.2.5 -o yt-spammer.tar.gz
 python3 get-pip.py
