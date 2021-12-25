042799e8c77603f840756460ad1592f148886ba5
lolpro11 <89674666+lolpro11@users.noreply.github.com>
2021-12-24 17:27:05 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 command -v python >/dev/null 2>&1 || { pacman -S python; dnf install python3; apt install python3; zypper install python3; emerge python;}
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 curl https://codeload.github.com/ThioJoe/YouTube-Spammer-Purge/tar.gz/refs/tags/v2.2.5 -o yt-spammer.tar.gz
-python get-pip.py
+python3 get-pip.py
 tar -xzf yt-spammer.tar.gz
 rm yt-spammer.tar.gz
 rm get-pip.py
