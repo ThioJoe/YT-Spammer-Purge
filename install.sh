841b27e6bb331f46ea14aed93f6d1808112578ce
lolpro11 <lolpro11@hostname.localdomain>
2021-12-24 19:17:57 -0800

    added required lib

--- install.sh
+++ install.sh
@@ -4,11 +4,12 @@ command -v python >/dev/null 2>&1 || { pacman -S python; dnf install python3; ap
 #install tkinter, a dependency
 apt-get install python3-tk; pacman -Syu tk; dnf install python3-tkinter; zypper install python3-tk
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-curl https://codeload.github.com/ThioJoe/YouTube-Spammer-Purge/tar.gz/refs/tags/v2.2.5 -o yt-spammer.tar.gz
 python3 get-pip.py
+# Uncomment if running this script alone, to also install the files for Youtube-Spammer-Purge
+: 'curl https://codeload.github.com/ThioJoe/YouTube-Spammer-Purge/tar.gz/refs/tags/v2.2.5 -o yt-spammer.tar.gz
 tar -xzf yt-spammer.tar.gz
 rm yt-spammer.tar.gz
+cd YouTube-Spammer-Purge-2.2.5/'
 rm get-pip.py
-cd YouTube-Spammer-Purge-2.2.5/
 pip install -r requirements.txt
 printf "Dependencies and Program installed!\nNow follow these instructions to get a client_secrets.json file!\nhttps://github.com/ThioJoe/YouTube-Spammer-Purge#instructions---obtaining-youtube-api-key"
