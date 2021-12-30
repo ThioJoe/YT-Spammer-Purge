a3bb42e0f6db841b3562ae039b2bf586e0efb7f2
lolpro11 <89674666+lolpro11@users.noreply.github.com>
2021-12-29 19:40:47 -0800

    Update install_full.sh

--- install_full.sh
+++ install_full.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
-#checks for python; if not installed will install it 
-command -v python >/dev/null 2>&1 || { py_installed = false;}
-if [ "$py_installed" = false ] ; then
-	if [[ -e /etc/debian_version ]]; then
-    		apt install python3
-	elif [[ -e /etc/fedora-release ]]; then
-    		dnf install python3
-  	elif [[ -e /etc/centos-release ]]; then
-		yum install tkinter
-	elif [[ -e /etc/arch-release ]]; then
-		pacman -Syu python3
-	else
-		echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS, Arch Linux system."
-		exit 1
-	fi
-fi
+#install python
 #install tkinter, a dependency
 if [[ -e /etc/debian_version ]]; then
+	apt install python3
 	apt-get install python3-tk
 elif [[ -e /etc/fedora-release ]]; then
 	dnf install python3
+	dnf install python3-tkinter
 elif [[ -e /etc/centos-release ]]; then
+	yum install -y python3
 	yum install tkinter
 elif [[ -e /etc/arch-release ]]; then
 	pacman -Syu python3
+	pacman -Syu tk
+else
+	echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS, Arch Linux system."
+	exit 1
 fi
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
@@ -34,4 +26,4 @@ rm yt-spammer.tar.gz
 cd YouTube-Spammer-Purge-2.2.5/
 rm get-pip.py
 pip install -r requirements.txt
-printf "Dependencies and Program installed!\nNow follow these instructions to get a client_secrets.json file!\nhttps://github.com/ThioJoe/YouTube-Spammer-Purge#instructions---obtaining-youtube-api-key"
+printf "Dependencies and Program installed!\nNow follow these instructions to get a client_secrets.json file!\nhttps://github.com/ThioJoe/YouTube-Spammer-Purge#instructions---obtaining-youtube-api-key\n"
