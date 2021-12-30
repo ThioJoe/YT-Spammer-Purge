5b42888496597b648e3c9a0dc78ab59332c7f3b0
lolpro11 <lolpro11@hostname.localdomain>
2021-12-30 14:01:28 -0800

    changes

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 #install python
 #install tkinter, a dependency
 if [[ -e /etc/debian_version ]]; then
-	apt install python3
-	apt-get install python3-tk
+	apt install python3 python3-tk
 elif [[ -e /etc/fedora-release ]]; then
 	dnf install python3
 	dnf install python3-tkinter
@@ -11,19 +10,18 @@ elif [[ -e /etc/centos-release ]]; then
 	yum install -y python3
 	yum install tkinter
 elif [[ -e /etc/arch-release ]]; then
-	pacman -Syu python3
-	pacman -Syu tk
+	pacman -Sy python3 tk
 else
 	echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS, Arch Linux system."
 	exit 1
 fi
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
+sudo -u $USER curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo -u $USER python3 get-pip.py
 # Uncomment if running this script alone, to also install the files for Youtube-Spammer-Purge
-: 'curl https://codeload.github.com/ThioJoe/YouTube-Spammer-Purge/tar.gz/refs/tags/v2.2.5 -o yt-spammer.tar.gz
-tar -xzf yt-spammer.tar.gz
-rm yt-spammer.tar.gz
-cd YouTube-Spammer-Purge-2.2.5/'
-rm get-pip.py
-pip install -r requirements.txt
+: 'sudo -u $USER curl https://codeload.github.com/ThioJoe/YouTube-Spammer-Purge/tar.gz/refs/tags/v2.2.5 -o yt-spammer.tar.gz
+sudo -u $USER tar -xzf yt-spammer.tar.gz
+sudo -u $USER rm yt-spammer.tar.gz
+sudo -u $USER cd YouTube-Spammer-Purge-2.2.5/'
+sudo -u $USER rm get-pip.py
+sudo -u $USER pip -q install -r requirements.txt
 printf "Dependencies and Program installed!\nNow follow these instructions to get a client_secrets.json file!\nhttps://github.com/ThioJoe/YouTube-Spammer-Purge#instructions---obtaining-youtube-api-key\n"
