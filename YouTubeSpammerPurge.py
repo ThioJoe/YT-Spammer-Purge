6681cc6bbf3da2019f23a9b4768a5c05aa329b83
Panos-Jr <84411578+Panos-Jr@users.noreply.github.com>
2022-01-02 22:44:28 +0000

    Update YouTubeSpammerPurge.py

--- YouTubeSpammerPurge.py
+++ YouTubeSpammerPurge.py
@@ -105,6 +105,7 @@ def get_authenticated_service():
 
   # Check if client_secrets.json file exists, if not give error
   if not os.path.exists(CLIENT_SECRETS_FILE):
+    CURR_DIR = os.path.dirname(os.path.realpath(__file__)
     LIST_DIR = os.listdir(CURR_DIR) #list current directory, array.
     for dir in LIST_DIR:
       if dir.endswith('.json'):
