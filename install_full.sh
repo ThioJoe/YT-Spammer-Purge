45a992b0fc12762b142bd97d72bf206bb7345468
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2022-01-06 13:21:25 -0700

    Update Install.sh to 2.7.4 on stable channel

--- install_full.sh
+++ install_full.sh
@@ -17,10 +17,10 @@ fi
 sudo -u $USER curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo -u $USER python3 get-pip.py
 # Uncomment if running this script alone, to also install the files for YT-Spammer-Purge
-sudo -u $USER curl https://codeload.github.com/ThioJoe/YT-Spammer-Purge/tar.gz/refs/tags/v2.7.3 -o yt-spammer.tar.gz
+sudo -u $USER curl https://codeload.github.com/ThioJoe/YT-Spammer-Purge/tar.gz/refs/tags/v2.7.4 -o yt-spammer.tar.gz
 sudo -u $USER tar -xzf yt-spammer.tar.gz
 sudo -u $USER rm yt-spammer.tar.gz
-sudo -u $USER cd YT-Spammer-Purge-2.7.3/
+sudo -u $USER cd YT-Spammer-Purge-2.7.4/
 sudo -u $USER rm get-pip.py
 sudo -u $USER pip install -r requirements.txt
 printf "Dependencies and Program installed!\nNow follow these instructions to get a client_secrets.json file!\nhttps://github.com/ThioJoe/YT-Spammer-Purge/wiki/Instructions:-Obtaining-an-API-Key\n"
