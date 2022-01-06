115d7cd0fb383b199d1aae3f21ec8578a560c922
Birk Birkner <birkner@bbirkner.de>
2022-01-06 21:51:47 +0100

    Altered install_full.sh and install.sh to work.
    Made it work wihout needing to run as sudo.
    Changed to install pip via the package manager and removed everything with get-pip.
    Made pip3 to install in python user install directory.
    Fixed centos tkinter dependency to python3-tkinter.
    Switched arch pacman command to not do a full system upgrade.
    
    Updated install.sh as well

--- install.sh
+++ install.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
-#install python
-#install tkinter, a dependency
+#install python and tkinter, a dependency
 if [[ -e /etc/debian_version ]]; then
-	apt install python3 python3-tk
+	sudo apt install python3 python3-tk python3-pip
 elif [[ -e /etc/fedora-release ]]; then
-	dnf install python3
-	dnf install python3-tkinter
+	sudo dnf install python3 python3-tkinter python3-pip
 elif [[ -e /etc/centos-release ]]; then
-	yum install -y python3
-	yum install tkinter
+	sudo yum install -y python3 python3-tkinter python3-pip
 elif [[ -e /etc/arch-release ]]; then
-	pacman -Sy python3 tk
+	sudo pacman -S python3 tk python-pip
 else
 	echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS, Arch Linux system."
 	exit 1
 fi
-sudo -u $USER curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo -u $USER python3 get-pip.py --no-warn-script-location
 # Uncomment if running this script alone, to also install the files for YT-Spammer-Purge
-: 'sudo -u $USER curl https://codeload.github.com/ThioJoe/YT-Spammer-Purge/tar.gz/refs/tags/v2.7.3 -o yt-spammer.tar.gz
-sudo -u $USER tar -xzf yt-spammer.tar.gz
-sudo -u $USER rm yt-spammer.tar.gz
-sudo -u $USER cd YT-Spammer-Purge-2.7.3/'
-sudo -u $USER rm get-pip.py
-sudo -u $USER pip -q install -r requirements.txt
+: 'curl https://codeload.github.com/ThioJoe/YT-Spammer-Purge/tar.gz/refs/tags/v2.7.4 -o yt-spammer.tar.gz
+ tar -xzf yt-spammer.tar.gz
+ rm yt-spammer.tar.gz'
+ cd YT-Spammer-Purge-2.7.4/
+ pip3 install -r requirements.txt --user
 printf "Dependencies and Program installed!\nNow follow these instructions to get a client_secrets.json file!\nhttps://github.com/ThioJoe/YT-Spammer-Purge/wiki/Instructions:-Obtaining-an-API-Key\n"
