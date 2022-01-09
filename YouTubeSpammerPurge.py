c560a561ceeed3ab9c4e17919590989f4abc3854
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2022-01-08 18:23:42 -0700

    Update Script - Version Number 2.9.0 for release

--- YouTubeSpammerPurge.py
+++ YouTubeSpammerPurge.py
@@ -36,7 +36,7 @@
 ### IMPORTANT:  I OFFER NO WARRANTY OR GUARANTEE FOR THIS SCRIPT. USE AT YOUR OWN RISK.
 ###             I tested it on my own and implemented some failsafes as best as I could,
 ###             but there could always be some kind of bug. You should inspect the code yourself.
-version = "2.9.0-Beta2"
+version = "2.9.0"
 configVersion = 17
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
