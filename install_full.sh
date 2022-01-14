9e0433dcc677f7a999e50925c4409f3c64649871
Knirps1 <52383078+Knirps1@users.noreply.github.com>
2022-01-15 00:59:15 +0100

    Fix linux installation scripts (#342)
    * Altered install_full.sh to work.
    
    Made it work wihout needing to run as sudo.
    Changed to install pip via the package manager and removed everything with get-pip.
    Made pip3 to install in python user install directory.
    Fixed centos tkinter dependency to python3-tkinter.
    Switched arch pacman command to not do a full system upgrade.
    
    * Altered install_full.sh and install.sh to work.
    
    Made it work wihout needing to run as sudo.
    Changed to install pip via the package manager and removed everything with get-pip.
    Made pip3 to install in python user install directory.
    Fixed centos tkinter dependency to python3-tkinter.
    Switched arch pacman command to not do a full system upgrade.
    
    Updated install.sh as well
    
    * Forgot to change pip to install pip modules in user directory.
    
    * Fixed error on version check where it would always exit with status 1.
    
    * Fixed anpther error with the distro check.
    
    * Revert "Fixed anpther error with the distro check."
    
    This reverts commit 2cc074eff8260dfb32907ca1a5441756c721995d.
    
    * Revert "Fixed error on version check where it would always exit with status 1."
    
    This reverts commit e786f2faec41a462b1bd9782d66a16596764eb27.
    
    * Several fixes in Linux install scripts
    
    Fixed Distro check which would always abort.
    Fixed jq removal wich was the wrong way round.
    
    Co-authored-by: Birk Birkner <birkner@bbirkner.de>

--- install_full.sh
+++ install_full.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-command -v jq >/dev/null 2>&1 || { JQ=0; }
+
+if [[ -e /etc/debian_version ]] || [[ -e /etc/fedora-release ]] || [[ -e  /etc/centos-release ]] || [[ -e /etc/arch-release ]]; then
+	:
+else
+	echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS, or Arch Linux system."
+	exit 1
+fi
+
+
+command -v jq >/dev/null 2>&1 && { JQ=0; }
 #install python
 if ! command -v python3 &> /dev/null; then
 	if [[ -e /etc/debian_version ]]; then
@@ -9,36 +18,36 @@ if ! command -v python3 &> /dev/null; then
 	elif [[ -e /etc/centos-release ]]; then
 		sudo yum install -y python3
 	elif [[ -e /etc/arch-release ]]; then
-		sudo pacman -Sy python3
-	else
-		echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS, or Arch Linux system."
-		exit 1
+		sudo pacman -S python3
 	fi
 else
-	echo "Skipping installing python, as python is preinstalled"
+	echo "Skipping installing python, as python is preinstalled."
 fi
 #install tkinter, a dependency
 #install jq, needed to get version number
 if [[ -e /etc/debian_version ]]; then
-	sudo apt install python3-tk jq
+	sudo apt install python3-tk jq python3-pip
 elif [[ -e /etc/fedora-release ]]; then
-	sudo dnf install python3-tkinter jq
+	sudo dnf install python3-tkinter jq python3-pip
 elif [[ -e /etc/centos-release ]]; then
-	sudo yum install -y tkinter epel-release jq
+	rpm -q epel-release &> /dev/null || EPEL=0 
+	sudo yum install -y python3-tkinter epel-release python3-pip
+	sudo yum install -y jq
+	[[ $EPEL -eq 0 ]] && sudo yum remove -y epel-release
 elif [[ -e /etc/arch-release ]]; then
-	sudo pacman -Sy tk jq
+	sudo pacman -S --needed tk jq
 fi
-command -v pip >/dev/null 2>&1 || { curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py; python3 get-pip.py --no-warn-script-location; rm get-pip.py; }
+
 TAG=$(curl https://api.github.com/repos/ThioJoe/YT-Spammer-Purge/releases/latest -s | jq .name -r)
 if  [[ $JQ -ne 0 ]]; then
 	if [[ -e /etc/debian_version ]]; then
-		apt purge jq
+		sudo apt purge jq
 	elif [[ -e /etc/fedora-release ]]; then
-		dnf remove jq
+		sudo dnf remove jq
 	elif [[ -e /etc/centos-release ]]; then
-		yum remove jq epel-release
+		sudo yum remove jq
 	elif [[ -e /etc/arch-release ]]; then
-		pacman -Rs jq
+		sudo pacman -Rs jq
 	fi
 else
 	echo "Did not uninstall jq as it was preinstalled before running this script."
@@ -49,5 +58,5 @@ curl https://codeload.github.com/ThioJoe/YT-Spammer-Purge/tar.gz/refs/tags/v${TA
 tar -xzf yt-spammer.tar.gz
 rm yt-spammer.tar.gz
 cd YouTube-Spammer-Purge-${TAG}/
-bash -c "pip install -r requirements.txt"
+bash -c "pip3 install -r requirements.txt --user"
 printf "Dependencies and Program installed!\nNow follow these instructions to get a client_secrets.json file!\nhttps://github.com/ThioJoe/YouTube-Spammer-Purge#instructions---obtaining-youtube-api-key\n"
