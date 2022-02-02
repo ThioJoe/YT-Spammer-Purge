6dbfef651dd4d8200952570018cbdc9ba7a6ea4a
Kendall Pinto <github@kendalldoescoding.gq>
2022-02-02 17:44:09 +0530

    list short url for bug report after line 1268 with long url

--- YTSpammerPurge.py
+++ YTSpammerPurge.py
@@ -1266,6 +1266,7 @@ def main():
         print("\nThe deletion functionality was not enabled. Cannot delete or report comments.")
         print("Possible Cause: You're scanning someone elses video with a non-supported filter mode.\n")
         print("If you think this is a bug, you may report it on this project's GitHub page: https://github.com/ThioJoe/YT-Spammer-Purge/issues")
+	print("Short Url: https://tjoe.io/bug-report")
         if config['auto_close'] == True:
           print("\nAuto-close enabled in config. Exiting in 5 seconds...")
           time.sleep(5)
