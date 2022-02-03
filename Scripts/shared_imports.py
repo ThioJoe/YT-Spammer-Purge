1096553ae47ae590d22eacf7af63f876ca94740a
EthanHindmarsh <ethan@ethan.lol>
2022-02-02 21:28:33 -0600

    Revert "disabling colors = dependency hell"
    This reverts commit 0573a6945d5b48d577e5443d22545eca2b195e3f.

--- Scripts/shared_imports.py
+++ Scripts/shared_imports.py
@@ -3,12 +3,25 @@ import sys
 import traceback
 import re
 
+from colorama import init, Fore as F, Back as B, Style as S
+
+
+class BlankFore:
+    # If the user wants to disable colors, set all of them = nothing
+    BLACK = BLUE = CYAN = GREEN = LIGHTBLACK_EX = LIGHTBLUE_EX = LIGHTCYAN_EX = LIGHTGREEN_EX = LIGHTMAGENTA_EX = LIGHTRED_EX = LIGHTWHITE_EX = LIGHTYELLOW_EX = MAGENTA = RED = RESET = WHITE = YELLOW = R = ''
+
+class BlankBack:
+    # If the user wants to disable colors, set all of them = nothing
+    BLACK = BLUE = CYAN = GREEN = LIGHTBLACK_EX = LIGHTBLUE_EX = LIGHTCYAN_EX = LIGHTGREEN_EX = LIGHTMAGENTA_EX = LIGHTRED_EX = LIGHTWHITE_EX = LIGHTYELLOW_EX = MAGENTA = RED = RESET = WHITE = YELLOW = R = ''
+
+
+class BlankStyle:
+    # If the user wants to disable colors, set them all = nothing
+    BRIGHT = DIM = NORMAL = RESET_ALL = R = ''
+
 
 
 # Global Hardcoded Constants
 RESOURCES_FOLDER_NAME = "SpamPurge_Resources"
 
-from .colors import *
-
-
-__all__ = ['os', 'sys', 're', 'traceback', 'F', 'B', 'S', 'init', 'RESOURCES_FOLDER_NAME']
+__all__ = ['os', 'sys', 're', 'traceback', 'F', 'B', 'S', 'init', 'RESOURCES_FOLDER_NAME', 'BlankFore', 'BlankBack', 'BlankStyle']
