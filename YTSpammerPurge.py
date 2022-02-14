d5d943a3f9752c00dd190536453db04d0200ff7e
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2022-02-13 20:51:36 -0700

    Fix string comparison on bool var
    banChoice was not converted to string before having .lower() applied, so crashed

--- YTSpammerPurge.py
+++ YTSpammerPurge.py
@@ -36,7 +36,7 @@
 ### IMPORTANT:  I OFFER NO WARRANTY OR GUARANTEE FOR THIS SCRIPT. USE AT YOUR OWN RISK.
 ###             I tested it on my own and implemented some failsafes as best as I could,
 ###             but there could always be some kind of bug. You should inspect the code yourself.
-version = "2.15.3"
+version = "2.15.4"
 configVersion = 26
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
