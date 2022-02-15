35a4999398eaf340e45fa6387bd5e40e04a981cd
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2022-02-14 17:16:59 -0700

    Normalize confusablesCustom config paths
    It was causing a crash when running the exe because it wasn't converting the forward slash.

--- Scripts/confusablesCustom/config.py
+++ Scripts/confusablesCustom/config.py
@@ -1,5 +1,13 @@
-CUSTOM_CONFUSABLE_PATH = "assets/custom_confusables.txt"
-CONFUSABLES_PATH = "assets/confusables.txt"
-CONFUSABLE_MAPPING_PATH = "assets/confusable_mapping.json"
+import os
+import sys
+
+def assets_path(relative_path):
+    if hasattr(sys, '_MEIPASS'): # If running as a pyinstaller bundle
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("Scripts/confusablesCustom/assets"), relative_path) # If running as script, specifies resource folder as /assets
+
+CUSTOM_CONFUSABLE_PATH = os.path.join(assets_path("custom_confusables.txt"))
+CONFUSABLES_PATH = os.path.join(assets_path("confusables.txt"))
+CONFUSABLE_MAPPING_PATH = os.path.join(assets_path("confusable_mapping.json"))
 MAX_SIMILARITY_DEPTH = 2
 NON_NORMAL_ASCII_CHARS = ['@']
