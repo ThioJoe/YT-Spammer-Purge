d7ec1c2823a85e69526b13a31cd531ea5bce42f3
micziz <99438936+micziz@users.noreply.github.com>
2022-02-17 10:19:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,62 +1,153 @@
 #!/bin/bash
 
-if [[ -e /etc/debian_version ]] || [[ -e /etc/fedora-release ]] || [[ -e  /etc/centos-release ]] || [[ -e /etc/arch-release ]]; then
-	:
-else
-	echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS, or Arch Linux system."
-	exit 1
-fi
+clear
+# Clear screen before running any commands
+
+REQUIREMENTS_INSTALLED=0
+
+install_fail () {
+    echo "Install Failed."
+    exit 1
+}
+
+install_debian () {
+    sudo apt install python3 python3-tk python3-pip git || install_fail
+}
+
+install_fedora () {
+    sudo dnf install python3 python3-tkinter python3-pip git || install_fail
+}
+
+install_centos () {
+    sudo yum install -y python3 || install_fail
+    rpm -q epel-release &> /dev/null || EPEL=0
+    sudo yum install -y python3-tkinter epel-release python3-pip git || install_fail
+    # Honestly not sure why it's installing epel and then uninstalling
+    [[ $EPEL -eq 0 ]] && sudo yum remove -y epel-release
+}
+
+install_arch () {
+    sudo pacman -S --needed python3 tk git && python3 -m ensurepip || install_fail
+}
+
+install_macos() {
+    echo "This script will install homebrew, if you do not wish to install homebrew, exit within 5 seconds..."
+    sleep 5
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # Install python3.10 & git or fail out
+    brew install python@3.10 && \
+    brew install git || \
+        install_fail
+}
+
+install_python_requirements () {
+    python3 -m pip install -q -r requirements.txt --user && \
+        echo "Python requirements installed." || exit 1
+    # Pip should give an error if it fails.
+}
+
+install_os_requirements () {
+    echo "YT-Spammer-Purge has a few requirements that you will need to install."
+    sleep 1
 
 
-command -v jq >/dev/null 2>&1 && { JQ=0; }
-#install python
-if ! command -v python3 &> /dev/null; then
-	if [[ -e /etc/debian_version ]]; then
-		sudo apt install python3
-	elif [[ -e /etc/fedora-release ]]; then
-		sudo dnf install python3
-	elif [[ -e /etc/centos-release ]]; then
-		sudo yum install -y python3
-	elif [[ -e /etc/arch-release ]]; then
-		sudo pacman -S python3
-	fi
-else
-	echo "Skipping installing python, as python is preinstalled."
-fi
-#install tkinter, a dependency
-#install jq, needed to get version number
-if [[ -e /etc/debian_version ]]; then
-	sudo apt install python3-tk jq python3-pip
-elif [[ -e /etc/fedora-release ]]; then
-	sudo dnf install python3-tkinter jq python3-pip
-elif [[ -e /etc/centos-release ]]; then
-	rpm -q epel-release &> /dev/null || EPEL=0 
-	sudo yum install -y python3-tkinter epel-release python3-pip
-	sudo yum install -y jq
-	[[ $EPEL -eq 0 ]] && sudo yum remove -y epel-release
-elif [[ -e /etc/arch-release ]]; then
-	sudo pacman -S --needed tk jq
+    # Check for known OS's
+    INSTALLED=0
+
+    case "$(uname -s)" in
+        Darwin*) install_macos && INSTALLED=1 || install_fail;;
+        # Mac
+        #CYGWIN*) do_something;;
+        # Cygwin Emulator -- Unimplemented
+        #MINGW*) do_something;;
+        # MINGW Emulator -- Unimplemented
+    esac
+
+    [[ -e /etc/debian_version ]] && install_debian && INSTALLED=1
+
+    [[ -e /etc/fedora-release ]] && install_fedora && INSTALLED=1
+
+    [[ -e  /etc/centos-release ]] && install_centos && INSTALLED=1
+
+    [[ -e /etc/arch-release ]] && install_arch && INSTALLED=1
+
+    [[ $INSTALLED -eq 0 ]] && printf "You are on an unknown system. You will have to install the required packages manually.\nContributions are welcome to add support for your system:\nhttps://github.com/ThioJoe/YT-Spammer-Purge" && exit 1
+
+}
+
+install_latest_release () {
+    echo "We are now going to download the code for YT-Spammer-Purge."
+    sleep 1
+    git clone https://github.com/ThioJoe/YT-Spammer-Purge
+    cd YT-Spammer-Purge
+    git checkout -q -m $(git describe --abbrev=0 --tags)
+    # Go to latest release
+}
+
+install_MAIN () {
+    clear
+    echo "We will now install YT-Spammer-Purge. If this is not what you intend to do, exit within 5 seconds..."
+    sleep 5
+    echo "Installing."
+    # Check what OS we're running on
+
+    # Check if we already installed the requirements -- git is needed to check for which mode to run.
+    [[ $REQUIREMENTS_INSTALLED -eq 0 ]] && install_os_requirements
+
+    echo "--------------------------"
+
+    install_latest_release
+
+    # Since we've gotten python3 installed:
+
+    echo "--------------------------"
+    install_python_requirements
+
+    # Done!
+
+    printf "Dependencies and Program installed into .\YT-Spammer-Purge!\nNow follow these instructions to get a client_secrets.json file!\nhttps://github.com/ThioJoe/YT-Spammer-Purge/wiki/Instructions:-Obtaining-an-API-Key\n\nYou may run this script again inside your installation to update.\n"
+    exit 1
+}
+
+update () {
+    clear
+    echo "We will now update YT-Spammer-Purge. If this is not what you intend to do, exit within 5 seconds..."
+    sleep 5
+    echo "Continuing."
+    echo "Current version is $(git describe --abbrev=0 --tags)"
+    echo "Updating..."
+
+
+    git fetch origin
+    echo "Latest version is $(git describe origin --abbrev=0 --tags)"
+    echo "Updating to this version."
+    git checkout -q -m $(git describe origin --abbrev=0 --tags)
+
+
+    echo "--------------------------"
+    echo "Updated!"
+    echo "Report any bugs to TJoe.io/bug-report"
+    exit 1
+}
+
+git_missing () {
+    echo "It looks like you downloaded a .zip of YT-Spammer-Purge"
+    echo "Automated updates do not work on these versions, but you may download the latest version of YT-Spammer-Purge using this script."
+    echo "If you choose to re-download the latest verion of YT-Spammer-Purge using this script, automated updates will be re-enabled."
+    echo "The latest YT-Spammer-Purge with automated updates will be downloaded to a sub-directory of the same name."
+    echo "If you would not like to install YT-Spammer-Purge using this script, close this script within the next 15 seconds..."
+    sleep 15
+    install_MAIN
+}
+
+if ! command -v git &> /dev/null
+then
+    echo "You are missing some required packages to run this script."
+    install_os_requirements
+    REQUIREMENTS_INSTALLED=1
 fi
 
-TAG=$(curl https://api.github.com/repos/ThioJoe/YT-Spammer-Purge/releases/latest -s | jq .name -r)
-if  [[ $JQ -ne 0 ]]; then
-	if [[ -e /etc/debian_version ]]; then
-		sudo apt purge jq
-	elif [[ -e /etc/fedora-release ]]; then
-		sudo dnf remove jq
-	elif [[ -e /etc/centos-release ]]; then
-		sudo yum remove jq
-	elif [[ -e /etc/arch-release ]]; then
-		sudo pacman -Rs jq
-	fi
-else
-	echo "Did not uninstall jq as it was preinstalled before running this script."
-fi
+[[ -e YTSpammerPurge.py ]] && git_missing
 
-# Uncomment if running this script alone, to also install the files for Youtube-Spammer-Purge
-: 'curl https://codeload.github.com/ThioJoe/YT-Spammer-Purge/tar.gz/refs/tags/v${TAG} -o yt-spammer.tar.gz
-tar -xzf yt-spammer.tar.gz
-rm yt-spammer.tar.gz
-cd YT-Spammer-Purge-${TAG}/'
-bash -c "pip3 install -r requirements.txt --user"
-printf "Dependencies and Program installed!\nNow follow these instructions to get a client_secrets.json file!\nhttps://github.com/ThioJoe/YT-Spammer-Purge/wiki/Instructions:-Obtaining-an-API-Key\n"
+git remote get-url origin > /dev/null && update || install_MAIN
+# If get-url succeeds, update, else install
