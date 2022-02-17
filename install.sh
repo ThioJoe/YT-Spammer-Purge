954cb0b46893082a1bfc45a888d004b40c99d04b
EthanHindmarsh <ethan@ethan.lol>
2022-02-16 21:09:53 -0600

    Implement jumping to latest version and MacOS

--- install.sh
+++ install.sh
@@ -3,77 +3,132 @@
 clear
 # Clear screen before running any commands
 
+REQUIREMENTS_INSTALLED=0
 
 install_debian () {
     sudo apt install python3 python3-tk python3-pip git
 }
 
 install_fedora () {
-	sudo dnf install python3 python3-tkinter python3-pip git
+    sudo dnf install python3 python3-tkinter python3-pip git
 }
 
 
 install_centos () {
-	sudo yum install -y python3
-	rpm -q epel-release &> /dev/null || EPEL=0
-	sudo yum install -y python3-tkinter epel-release python3-pip git
-	# Honestly not sure why it's installing epel and then uninstalling
+    sudo yum install -y python3
+    rpm -q epel-release &> /dev/null || EPEL=0
+    sudo yum install -y python3-tkinter epel-release python3-pip git
+    # Honestly not sure why it's installing epel and then uninstalling
     [[ $EPEL -eq 0 ]] && sudo yum remove -y epel-release
 }
 
 install_arch () {
-	sudo pacman -S --needed python3 tk git
+    sudo pacman -S --needed python3 tk git
     python3 -m ensurepip
+}
+
+install_macos() {
+    echo "This script will install homebrew, if you do not wish to install homebrew, exit within 5 seconds..."
+    sleep 5
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install python@3.10
+    brew install git
 }
 
 install_python_requirements () {
     python3 -m pip install -r requirements.txt --user && \
     echo "Python requirements installed" || \
-    echo "Python requirements did not install successfully"
+    echo "Python requirements did not install successfully" && exit 1
 }
 
 install_os_requirements () {
+    echo "YT-Spammer-Purge has a few requirements that you will need to install."
+    sleep 1
+
     # Check for known OS's
     INSTALLED=0
 
-    [[ -e /etc/debian_version ]] && install_debian && INSTALLED=1
+    case "$(uname -s)" in
+        Darwin*) install_macos && INSTALLED=1 || echo "Install failed" && exit 1;;
+        # Mac
+        #CYGWIN*) do_something;;
+        # Cygwin Emulator -- Unimplemented
+        #MINGW*) do_something;;
+        # MINGW Emulator -- Unimplemented
+    esac
 
-    [[ -e /etc/fedora-release ]] && install_fedora && INSTALLED=1
+    [[ -e /etc/debian_version ]] && install_debian && INSTALLED=1 || echo "Install failed" && exit 1
 
-    [[ -e  /etc/centos-release ]] && install_centos && INSTALLED=1
+    [[ -e /etc/fedora-release ]] && install_fedora && INSTALLED=1 || echo "Install failed" && exit 1
 
-    [[ -e /etc/arch-release ]] && install_arch && INSTALLED=1
+    [[ -e  /etc/centos-release ]] && install_centos && INSTALLED=1 || echo "Install failed" && exit 1
+
+    [[ -e /etc/arch-release ]] && install_arch && INSTALLED=1 || echo "Install failed" && exit 1
 
     [[ $INSTALLED -eq 0 ]] && printf "You are on an unknown system. You will have to install the required packages manually.\nContributions are welcome to add support for your system:\nhttps://github.com/ThioJoe/YT-Spammer-Purge" && exit 1
 
 }
 
+install_latest_release () {
+    echo "We are now going to download the code for YT-Spammer-Purge."
+    sleep 1
+    git clone https://github.com/ThioJoe/YT-Spammer-Purge
+    cd YT-Spammer-Purge
+    git checkout -m $(git describe --abbrev=0 --tags)
+    # Go to latest release
+}
+
 install_MAIN () {
+    echo "We will now install YT-Spammer-Purge. If this is not what you intend to do, exit within 5 seconds..."
+    sleep 5
+    echo "Installing."
     clear
     # Check what OS we're running on
-    echo "YT-Spammer-Purge has a few requirements that you will need to install."
 
-    install_os_requirements
+    # Check if we already installed the requirements -- git is needed to check for which mode to run.
+    [[$REQUIREMENTS_INSTALLED -eq 0]] && install_os_requirements
+    clear
 
-    # If we've gotten python3 installed:
+    install_latest_release
+    clear
+
+    # Since we've gotten python3 installed:
 
     install_python_requirements
+    clear
 
     # Done!
 
-    printf "Dependencies and Program installed!\nNow follow these instructions to get a client_secrets.json file!\nhttps://github.com/ThioJoe/YT-Spammer-Purge/wiki/Instructions:-Obtaining-an-API-Key\n"
+    printf "Dependencies and Program installed!\nNow follow these instructions to get a client_secrets.json file!\nhttps://github.com/ThioJoe/YT-Spammer-Purge/wiki/Instructions:-Obtaining-an-API-Key\n\nYou may run this script again in the future to update."
+    exit 1
 }
 
 update () {
-    echo "update here"
+    echo "We will now update YT-Spammer-Purge. If this is not what you intend to do, exit within 5 seconds..."
+    sleep 5
+    clear
+
+    echo "Current version is $(git describe --abbrev=0 --tags)"
+    echo "Updating..."
+
+
+    git fetch origin
+    echo "Latest version is $(git describe --abbrev=0 --tags)"
+    git checkout -m $(git describe --abbrev=0 --tags)
+    sleep 1
+    clear
+
+    echo "Updated!"
+    echo "Report any bugs to TJoe.io/bug-report"
+    exit 1
 }
 
 if ! command -v git &> /dev/null
 then
-    echo "You are missing some required packages."
+    echo "You are missing some required packages to run this script."
     install_os_requirements
+    REQUIREMENTS_INSTALLED=1
 fi
 
 git remote get-url origin > /dev/null && update || install_MAIN
-
-
+# If get-url succeeds, update, else install
