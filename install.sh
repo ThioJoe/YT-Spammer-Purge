10b5e305b972eb214a460fce15a8bc7657b0159e
Daniel Ziegenberg <daniel@ziegenberg.at>
2022-02-17 15:54:32 +0100

    merging install.sh and install_full.sh and improving the instal script
    This commit removes jq from the dependecies, getting tarball with system tools:
    
    There is no need to install jq just to parse the API response. This can
    be done with already available system tools like grep. Also extracting
    the tarball on-the-fly, leaving nothin behind if download succeeds, but
    extracting fails.
    
    This commit add options and usage message:
    
    This commit adds an option to only install the dependecies, making a
    separate install.sh superfluous. This reduces duplicated code and makes
    maintanence easier. Now you have to fix bugs only in one place.
    
    Other improvements:
    - use "cd ... || exit" in case cd fails
    - double quote to prevent globbing and word splitting
    - use `#!/usr/bin/env bash` - because env command allows to run a program in a modified environment
    - negating the test for checking linux system so we can save the else
    
    Signed-off-by: Daniel Ziegenberg <daniel@ziegenberg.at>

--- install.sh
+++ install.sh
@@ -1,20 +1,48 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ -e /etc/debian_version ]] || [[ -e /etc/fedora-release ]] || [[ -e  /etc/centos-release ]] || [[ -e /etc/arch-release ]]; then
-	:
-else
+# Installer script for The YouTube Spammer Purge application
+
+print_usage() {
+   # Display Usage
+   echo "Usage: $0 [-h] [-d]"
+   echo
+   echo "Installer script for The YouTube Spammer Purge application."
+   echo
+   echo "Options:"
+   echo "    -d     Only install dependencies."
+   echo "    -h     Print this Help."
+   echo
+}
+
+DEPS_ONLY=0
+
+# Get the options
+while getopts ":hd" option; do
+	case $option in
+		h)  # display Help
+			print_usage
+			exit;;
+		d)  # Install dependencies only
+			DEPS_ONLY=1;;
+		\?) # Invalid option
+			echo "Error: Invalid option. See option -h for help."
+			exit 1 ;;
+	esac
+done
+
+
+# Running this installer is only supported on specific systems.
+if ! { [[ -e /etc/debian_version ]] || [[ -e /etc/fedora-release ]] || [[ -e  /etc/centos-release ]] || [[ -e /etc/arch-release ]]; }; then
 	echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS, or Arch Linux system."
 	exit 1
 fi
 
-
-command -v jq >/dev/null 2>&1 && { JQ=0; }
 #install python
 if ! command -v python3 &> /dev/null; then
 	if [[ -e /etc/debian_version ]]; then
 		sudo apt install python3
 	elif [[ -e /etc/fedora-release ]]; then
-		sudo dnf install python3
+		sudo dnf --assumeyes --quiet install python3
 	elif [[ -e /etc/centos-release ]]; then
 		sudo yum install -y python3
 	elif [[ -e /etc/arch-release ]]; then
@@ -24,39 +52,27 @@ else
 	echo "Skipping installing python, as python is preinstalled."
 fi
 #install tkinter, a dependency
-#install jq, needed to get version number
+#install GCC and Python3 headers on Fedora, since python-Levenshtein needs to be compiled there.
 if [[ -e /etc/debian_version ]]; then
-	sudo apt install python3-tk jq python3-pip
+	sudo apt install python3-tk python3-pip
 elif [[ -e /etc/fedora-release ]]; then
-	sudo dnf install python3-tkinter jq python3-pip
+	sudo dnf --assumeyes --quiet install python3-tkinter python3-pip python3-devel gcc
 elif [[ -e /etc/centos-release ]]; then
-	rpm -q epel-release &> /dev/null || EPEL=0 
-	sudo yum install -y python3-tkinter epel-release python3-pip
-	sudo yum install -y jq
+	rpm -q epel-release &> /dev/null || EPEL=0
+	sudo yum install -y python3-tkinter epel-release python3-pip python3-devel gcc
+	sudo yum install -y
 	[[ $EPEL -eq 0 ]] && sudo yum remove -y epel-release
 elif [[ -e /etc/arch-release ]]; then
-	sudo pacman -S --needed tk jq
+	sudo pacman -S --needed tk
 fi
 
-TAG=$(curl https://api.github.com/repos/ThioJoe/YT-Spammer-Purge/releases/latest -s | jq .name -r)
-if  [[ $JQ -ne 0 ]]; then
-	if [[ -e /etc/debian_version ]]; then
-		sudo apt purge jq
-	elif [[ -e /etc/fedora-release ]]; then
-		sudo dnf remove jq
-	elif [[ -e /etc/centos-release ]]; then
-		sudo yum remove jq
-	elif [[ -e /etc/arch-release ]]; then
-		sudo pacman -Rs jq
-	fi
-else
-	echo "Did not uninstall jq as it was preinstalled before running this script."
+if [[ DEPS_ONLY -eq 0 ]]; then
+	TAG=$(curl https://api.github.com/repos/ThioJoe/YT-Spammer-Purge/releases/latest | grep -oP '(?<="tag_name": ")[^"]*')
+	location=YT-Spammer-Purge-"${TAG}"
+	curl https://codeload.github.com/ThioJoe/YT-Spammer-Purge/tar.gz/refs/tags/"${TAG}" | tar -xz -C "$location" --strip-components 1
+	cd "$location" || exit
+	bash -c "pip3 install -r requirements.txt --user"
+	printf "Dependencies and Program installed!\nNow follow these instructions to get a client_secrets.json file!\nhttps://github.com/ThioJoe/YT-Spammer-Purge/wiki/Instructions:-Obtaining-an-API-Key\n"
 fi
 
-# Uncomment if running this script alone, to also install the files for Youtube-Spammer-Purge
-: 'curl https://codeload.github.com/ThioJoe/YT-Spammer-Purge/tar.gz/refs/tags/v${TAG} -o yt-spammer.tar.gz
-tar -xzf yt-spammer.tar.gz
-rm yt-spammer.tar.gz
-cd YT-Spammer-Purge-${TAG}/'
-bash -c "pip3 install -r requirements.txt --user"
-printf "Dependencies and Program installed!\nNow follow these instructions to get a client_secrets.json file!\nhttps://github.com/ThioJoe/YT-Spammer-Purge/wiki/Instructions:-Obtaining-an-API-Key\n"
+exit 0
