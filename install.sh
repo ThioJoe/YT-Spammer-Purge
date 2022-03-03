15f5799805501fe2e5495910a1f7d61116ebd810
SJang1 <git@sjang.dev>
2022-03-04 07:53:14 +0900

    added python-tk, some device needs install

--- install.sh
+++ install.sh
@@ -98,6 +98,7 @@ install_macos() {
     # Install python3.10 & git or fail out
     brew install python@3.10 && \
     brew install tcl-tk && \
+    brew install python-tk && \
     brew install git || \
         install_fail
 }
